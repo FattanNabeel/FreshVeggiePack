7647e7f79e83d8ee014d6a09155b9ae2750caec4
FattanNabeel <fattannabeel123@gmail.com>
2025-01-15 07:59:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function buyNow(productName) {
+    alert(`Anda memilih paket: ${productName}. Silakan hubungi kami untuk pemesanan lebih lanjut.`);
+    // Tambahkan fungsionalitas lainnya, seperti membuka WhatsApp atau formulir pemesanan.
+}
